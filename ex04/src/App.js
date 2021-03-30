047ac95e70dc4762e51225a346e19e4ab81f259d
hero-BA <suad.hero@gmail.com>
2021-03-31 00:00:22 +0200

    Modified App.js file

--- ex04/src/App.js
+++ ex04/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { Component } from 'react';
-import Table from "./Table"; // Add this line inside the src/App.js file
+import Table from './Table'; // Add this line inside the src/App.js file
 
 class App extends Component {
   state = {
